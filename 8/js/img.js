2cfb8978ef60b2057707e6f41c65157b35ab9a60
Keks <keks@htmlacademy.ru>
2022-07-19 20:36:19 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/img.js
@@ -0,0 +1,18 @@
+import {createSimilarComments} from './data.js';
+
+const listOfPhotos = document.querySelector('.pictures');
+const template = document
+  .querySelector('#picture')
+  .content.querySelector('.picture');
+const fragmentOfPhotos = document.createDocumentFragment();
+
+
+createSimilarComments.forEach(({url, likes, comments}) => {
+  const pictureElement = template.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  fragmentOfPhotos.appendChild(pictureElement);
+});
+
+listOfPhotos.appendChild(fragmentOfPhotos);
---
+++ 8/js/main.js
@@ -0,0 +1,6 @@
+import './utils.js';
+import './img.js';
+import './full-view.js';
+import './form.js';
+import { createPhotos } from './data.js';
+createPhotos();
